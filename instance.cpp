2c0526a48dbe36cc5b23e45d7383aa923beba102
laolizhennui <167614027+laolizhennui@users.noreply.github.com>
2024-10-14 18:20:34 +0800

    Create instance.cpp

---
+++ instance.cpp
@@ -0,0 +1,17 @@
+/*
+Copyright (C) 2024 我不精通C语言
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3.0 of the License, or
+(at your option) any later version.
+
+*/
+
+#include "TdPr-dev.hpp"
+
+int main(int argc, char *argv[]) {
+    BigInt res = power(BigInt(2), 63);
+    cout << res << endl;
+    return 0;
+}
